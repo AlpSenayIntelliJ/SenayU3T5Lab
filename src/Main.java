23667103e2693aa3f127dad604b289f697f046b1
Alp Senay <alps2789@nycstudents.net>
2022-11-02 12:34:18 -0400

    working...

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,25 @@
-package PACKAGE_NAME;public class Main {
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int p = 5;
+        int q = 0;
+
+        if (p > 0 && (p / q) == 2)
+        {
+            System.out.println("it’s true :)");
+        }
+        else
+        {
+            System.out.println("it’s false :(");
+        }
+
+
+
+
+
+    }
 }
+
+
+
